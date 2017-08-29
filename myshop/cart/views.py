124d5d5be9166423a4819cb3f1aeae355d19bd93
pycat <kkampardi@gmail.com>
2017-08-29 11:32:55 +0300

    Allow users to change quantities from the cart detail page #2

--- myshop/cart/views.py
+++ myshop/cart/views.py
@@ -29,4 +29,10 @@ def cart_remove(request, product_id):
 
 def cart_detail(request):
     cart = Cart(request)
+
+    """ allow users to change quantities fromt the cart detail page """
+    for item in cart:
+        item['update_quantity_form'] = CartAddProductForm(
+            initial={'quantity': item['quantity'],
+                     'update': True})
     return render(request, 'cart/detail.html', {'cart': cart})
